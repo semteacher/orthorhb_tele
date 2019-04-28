c6bef61874e5c5ecf36e6a2145871b1fbe9825f3
Andriy Semenets <semteacher@gmail.com>
2019-04-28 11:20:17 +0300

    reduce number of tables - main form table mus be used instead of assignment

--- table.sql
+++ table.sql
@@ -65,28 +65,6 @@ INSERT INTO `form_orthorhb_sensors` (`id`, `sensorname`, `sensordesc`, `sensorto
 (1, 'prototype01', NULL, NULL),
 (2, 'prototype02', NULL, NULL);
 
--- --------------------------------------------------------
-
---
--- Table structure for table `form_orthorhb_sensor_assignment`
---
-
-CREATE TABLE `form_orthorhb_sensor_assignment` (
-  `id` bigint(20) NOT NULL,
-  `formid` bigint(20) DEFAULT NULL,
-  `patientid` bigint(20) NOT NULL,
-  `encounterid` bigint(20) DEFAULT NULL,
-  `sensorid` int(11) NOT NULL,
-  `assigndate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `form_orthorhb_sensor_assignment`
---
-
-INSERT INTO `form_orthorhb_sensor_assignment` (`id`, `formid`, `patientid`, `encounterid`, `sensorid`, `assigndate`) VALUES
-(1, NULL, 1, 1, 1, '2019-03-31 14:58:39'),
-(3, NULL, 2, 2, 2, '2019-03-31 15:04:10');
 
 -- --------------------------------------------------------
 
@@ -97,8 +75,8 @@ INSERT INTO `form_orthorhb_sensor_assignment` (`id`, `formid`, `patientid`, `enc
 CREATE TABLE `form_orthorhb_sensor_data` (
   `id` bigint(20) NOT NULL,
   `sensorid` int(11) NOT NULL,
-  `assignid` bigint(20) DEFAULT NULL,
-  `timerec` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `formid` bigint(20) DEFAULT NULL,
+  `timerec` BIGINT(10) DEFAULT NULL,
   `datavalue` text COLLATE utf8mb4_unicode_ci
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
@@ -106,11 +84,11 @@ CREATE TABLE `form_orthorhb_sensor_data` (
 -- Dumping data for table `form_orthorhb_sensor_data`
 --
 
-INSERT INTO `form_orthorhb_sensor_data` (`id`, `sensorid`, `assignid`, `timerec`, `datavalue`) VALUES
-(1, 1, 1, '2019-04-07 16:12:48', '{\"angle\":\"88\",\"temp\":\"36\",\"o2\":\"100\",\"volume\":null}'),
-(5, 1, 1, '2019-04-07 16:28:07', '{\"angle\":\"80\",\"temp\":\"37\",\"o2\":\"90\",\"volume\":\"15\"}'),
-(6, 2, 3, '2019-04-07 16:30:00', '{\"angle\":\"68\",\"temp\":\"34\",\"o2\":\"90\",\"volume\":\"20\"}'),
-(8, 2, 3, '2019-04-07 16:30:58', '{\"angle\":\"71\",\"temp\":\"35\",\"o2\":\"87\",\"volume\":\"21\"}');
+INSERT INTO `form_orthorhb_sensor_data` (`id`, `sensorid`, `formid`, `timerec`, `datavalue`) VALUES
+(1, 1, 13, 1554362199, '{\"angle\":\"88\",\"temp\":\"36\",\"o2\":\"100\",\"volume\":null}'),
+(5, 1, 13, 1554362592, '{\"angle\":\"80\",\"temp\":\"37\",\"o2\":\"90\",\"volume\":\"15\"}'),
+(6, 2, 14, 1556006950, '{\"angle\":\"68\",\"temp\":\"34\",\"o2\":\"90\",\"volume\":\"20\"}'),
+(8, 2, 14, 1556007013, '{\"angle\":\"71\",\"temp\":\"35\",\"o2\":\"87\",\"volume\":\"21\"}');
 
 --
 -- Indexes for dumped tables
@@ -120,12 +98,6 @@ INSERT INTO `form_orthorhb_sensor_data` (`id`, `sensorid`, `assignid`, `timerec`
 -- Indexes for table `form_orthorhb_sensors`
 --
 ALTER TABLE `form_orthorhb_sensors`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `form_orthorhb_sensor_assignment`
---
-ALTER TABLE `form_orthorhb_sensor_assignment`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -144,11 +116,6 @@ ALTER TABLE `form_orthorhb_sensor_data`
 ALTER TABLE `form_orthorhb_sensors`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
---
--- AUTO_INCREMENT for table `form_orthorhb_sensor_assignment`
---
-ALTER TABLE `form_orthorhb_sensor_assignment`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `form_orthorhb_sensor_data`
